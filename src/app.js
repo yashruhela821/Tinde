d0b93d7ad9c9dfc239007ad6d9063cd00c1950be
yash ruhela <yashruhela1053@gmail.com>
2024-11-02 16:02:05 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req,res )=>{
+app.use("/test+/tt", (req,res )=>{
+    res.send("tt page hai ye ")
+});
+app.use("/tes?t", (req,res )=>{
     res.send("test page hai ye ")
 });
-app.use("/home",(req,res)=>{
+
+app.use("/ho*me",(req,res)=>{
     res.send("home home home  !!")
-})
+});
+app.use("/gola/:userid/:name/:AGE",(req,res)=>{
+    console.log(req.params);
+    res.send("hanji")
+});
 app.listen(9999, ()=>{
     console.log("server sunn rha hai!!")
 });
